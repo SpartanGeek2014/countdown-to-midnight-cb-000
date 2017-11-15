5324ffff5c9e2972365c3326c9c526ea73f2cc92
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-15 23:40:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,10 @@
 #write your code here
 x = 0
-def countdown
-  while x < 6
-    puts "#{{x} SECOND(s)!}"
-    x -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECONDS!"
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
+
