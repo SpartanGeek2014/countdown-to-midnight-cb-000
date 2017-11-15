b28a608f5432764ea93892cce77617577831d0a6
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-15 23:44:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,17 @@
 x = 0
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECONDS!"
+    puts "#{seconds_to_midnight} SECOND(S)!"
     sleep(1)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end 
